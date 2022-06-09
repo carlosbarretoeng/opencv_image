4b493f81efde9c4cd876a57fe2193d4649bf3185
Carlos Roberto Barreto Junior <carlosbarretoeng@users.noreply.github.com>
2022-06-09 01:48:12 +0000

    UP

--- main.py
+++ main.py
@@ -9,3 +9,11 @@ plt.savefig('img1.png')
 
 stabilizer.plot_transforms()
 plt.savefig('img2.png')
+
+stabilizer.stabilize(input_path='stable_video.avi', output_path='final_video.avi')
+
+stabilizer.plot_trajectory()
+plt.savefig('img3.png')
+
+stabilizer.plot_transforms()
+plt.savefig('img4.png')
